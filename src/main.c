58feee4c7b3254e6db8e4b1b64057ee310c046fd
Pyae Phyo Zaw <pzaw@c2r1s6.42singapore.sg>
2024-10-08 17:31:21 +0800

    pipex 2

--- src/main.c
+++ src/main.c
@@ -1,11 +1,22 @@
 #include "pipex.h"
 
-int	main(int argc,char *argv[])
+void	init(t_ppx *info, int argc, char **argv, char **envp)
 {
-	ft_printf("%d\n", argc);
+	info->envp = envp;
+	info->argv = argv;
+	info->argc = argc;
+	info->heredoc = ft_strncmp("here_doc", argv[1], 9);
+}
+
+
+int	main(int argc,char *argv[], char **envp)
+{
+	t_ppx info;
+
 	if (argc < 5)
 		return (ft_printf("Input command not enough!!"));
-	if (!ft_strncmp("here_doc", argv[1], 9))
+	else if (ft_strncmp("here_doc", argv[1], 9) && argc < 6)
 		return (ft_printf("It is here doc!\n"));
+	init(&info, argc, argv, envp);
 	return (0);
 }
